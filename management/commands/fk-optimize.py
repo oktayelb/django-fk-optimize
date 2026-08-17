c84e9c0659b06cce82e075f422d74b81caec909c
oktayelb <oktayelb@gmail.com>
2026-08-18 01:25:08 +0300

    scaffoling the command, flow is laid out

--- management/commands/fk-optimize.py
+++ management/commands/fk-optimize.py
@@ -1,8 +1,38 @@
 from django.core.management.base import BaseCommand
-
-
+from django.db.models import Model
+from django.apps.registry import apps
 
 class Command(BaseCommand):
 
     help = "Queryset optimizer tool for models containing foreign keys."
-    
+
+    def add_arguments(self, parser):
+
+        parser.add_argument("app.model", nargs="?", type=str , default= None)
+        parser.add_argument("--timeout", type=int)
+
+    def handle (self, *args,**options):
+
+
+        selection :str  = options["app.model"]
+        model_s :list[type[Model]] = [] 
+
+        if selection is None:
+            model_s = list(apps.get_models())
+            
+        elif "." in selection:
+            model_s.append(apps.get_model(selection))
+        else:
+            
+            for mdl in apps.get_app_config(selection).get_models():
+                model_s.append(mdl)
+
+
+       #right now eihter we threw an error, 
+       # or model_s has all of the models we need
+
+       # for every model:
+       # if  no fk, skip
+       # else find the more suitable operation (select_related/prefetch)
+       # return the results
+        
